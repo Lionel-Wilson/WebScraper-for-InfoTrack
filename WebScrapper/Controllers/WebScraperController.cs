276eb0e8bd63a1966c1485a37163b5250f0ad90d
Lionel Wilson <Lionel_Wilson@outlook.com>
2023-09-05 16:43:24 +0100

    initial commit

---
+++ WebScrapper/Controllers/WebScraperController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebScrapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebScraperController : ControllerBase
+    {
+        [HttpGet]
+        public string Get([FromBody] string keywords, string url)
+        {
+            
+            
+            return 'test';
+        }
+    }
+}
