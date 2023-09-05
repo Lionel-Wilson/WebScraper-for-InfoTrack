186c3c566f9a40b86ebfe2d45e6d4aacd29e3ad1
Lionel Wilson <Lionel_Wilson@outlook.com>
2023-09-05 19:07:10 +0100

    Create Search history and Search Controllers

---
+++ WebScrapper.API/WebScrapper.API/Controllers/SearchHistoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebScrapper.API.Controllers
+{
+    public class SearchHistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
